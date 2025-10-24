42e34502e03a8a22cd1b56d9409e5ec39ffce895
Dolken01 <dolken001@hotmail.com>
2025-10-24 15:54:19 +0200

    fixade lite html

--- individuell_uppgift/application/app.py
+++ individuell_uppgift/application/app.py
@@ -1,5 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import json
+
 
 app = Flask(__name__)
 
+@app.route("/") #Skapar route /
+def index():
+    return render_template("main.html")
 
+@app.route("/priser")#Skapar route /priser
+def prices():
+    return "Sida som ska visa priser"
