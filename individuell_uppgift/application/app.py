1b6978716e0f426c5a51f334d821bde4ceea626d
Dolken01 <dolken001@hotmail.com>
2025-10-26 17:43:03 +0100

    la till felhantering

--- individuell_uppgift/application/app.py
+++ individuell_uppgift/application/app.py
@@ -27,9 +27,24 @@ def api_el():
     day = request.form.get("day")
     area = request.form.get("area")
 
+    try: # Fångar upp om användaren skriver fel format
+        selected_date = datetime.strptime(f"{year}-{month}-{day}", "%Y-%m-%d")
+    except ValueError:
+        return "Ogiltigt datumformat.", 400
+
+    
+    min_date = datetime(2022, 11, 1) # Sätter minsta datum man kan söka på
+    max_date = datetime.now().date() + timedelta(days=1) # Sätter högsta datum man kan söka på
+
+    if selected_date < min_date: # Felmedalande om användaren skriver för lågt datum
+        return "För lågt datum. Du kan inte söka elpriser före 2022-11-01.", 400
+    if selected_date.date() > max_date: # Felmedelande om användaren skriver datum för långt fram
+        return "Du kan inte söka elpriser längre fram än imorgon.", 400
+
+
     el_url = f"https://www.elprisetjustnu.se/api/v1/prices/{year}/{month}-{day}_{area}.json"
-    json_data = requests.get(el_url)
-    data = json.loads(json_data.text)
+    json_data = requests.get(el_url) # Hämtar json
+    data = json.loads(json_data.text) # laddar json
     df = pd.DataFrame(data)
     df["time_start"] = pd.to_datetime(df["time_start"], errors="coerce")
     df["time_start"] = df["time_start"].dt.strftime("%H:%M")
