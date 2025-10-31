638a03f9f67af4b6e8317c51a735d49232aa5b4b
Dolken01 <dolken001@hotmail.com>
2025-10-31 15:51:51 +0100

    la till lite test

--- individuell_uppgift/tests/test_data.py
+++ individuell_uppgift/tests/test_data.py
@@ -1,1 +1,20 @@
 import pytest
+import flask
+import ssl
+import urllib
+import requests
+
+context = ssl._create_unverified_context()
+
+def test_Is_online_index(): # Testar om startsidan är online
+    assert urllib.request.urlopen("http://127.0.0.1:5000", context=context, timeout=10)
+
+def test_404_response(): 
+    response = requests.get("http://127.0.0.1:5000/abc")
+    assert response.status_code == 404
+
+
+
+
+
+
