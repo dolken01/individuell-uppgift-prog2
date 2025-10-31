a8d349b42109e87e849adbd7d3105ce0242fca12
Dolken01 <dolken001@hotmail.com>
2025-10-31 17:00:20 +0100

    la till mer kommentarer

--- individuell_uppgift/tests/test_data.py
+++ individuell_uppgift/tests/test_data.py
@@ -6,16 +6,19 @@ import requests
 
 context = ssl._create_unverified_context()
 
-def test_Is_online_index(): # Testar om startsidan är online
+def test_Is_online_index():
+    """ Testar om huvud sidan är online """
     assert urllib.request.urlopen("http://127.0.0.1:5000", context=context, timeout=10)
 
 
 
 def test_404_response(): 
+    """ Testar 404 felhantering """
     response = requests.get("http://127.0.0.1:5000/abc")
     assert response.status_code == 404
 
 def test_invalid_date(): 
+    """ Testar ogiltigt datumformat """
     response = requests.post("http://127.0.0.1:5000/priser", data={
         "year": "2024",
         "month": "aa", 
@@ -26,6 +29,7 @@ def test_invalid_date():
     assert "Ogiltigt datumformat" in response.text
 
 def test_valid_date():
+    """ Testar giltigt datumformat """
     response = requests.post("http://127.0.0.1:5000/priser", data={
         "year": "2025",
         "month": "10",
@@ -36,6 +40,7 @@ def test_valid_date():
     assert "SEK_per_kWh" in response.text
 
 def test_too_low_date():
+    """ Testar datum för långt bak """
     response = requests.post("http://127.0.0.1:5000/priser", data={
         "year": "2021",
         "month": "10",
